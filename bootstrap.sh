d3b444c078b0a81bf171b3183cd74ceb142c4580
Mardix <mardix@pylot.io>
2019-10-01 22:34:25 -0400

    Upd

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,41 +6,17 @@ apt-get install -y wget curl cron build-essential certbot git incron \
    python-certbot-nginx python-dev python-pip python-virtualenv \
    python3-dev python3-pip python3-virtualenv \
    uwsgi uwsgi-plugin-asyncio-python3 uwsgi-plugin-gevent-python \
-   uwsgi-plugin-python uwsgi-plugin-python3 uwsgi-plugin-tornado-python
+   uwsgi-plugin-python uwsgi-plugin-python3 uwsgi-plugin-tornado-python \ 
 apt-get update
 
+pip install ansible
+
+pip3 install bokku
+
 cd /tmp
-wget https://raw.githubusercontent.com/mardix/bokku/master/files/incron.conf
-wget https://raw.githubusercontent.com/mardix/bokku/master/files/nginx.conf
-wget https://raw.githubusercontent.com/mardix/bokku/master/files/uwsgi-bokku.service
+wget https://raw.githubusercontent.com/mardix/bokku/master/files/playbook.yml
 
-cp /tmp/nginx.conf /etc/nginx/sites-available/default
-cp /tmp/incron.conf /etc/incron.d/bokku
-cp /tmp/uwsgi-bokku.service /etc/systemd/system/
-ln -s `which uwsgi` /usr/local/bin/uwsgi-bokku
-
-systemctl restart incron
-systemctl restart nginx
-systemctl disable uwsgi
-systemctl enable uwsgi-bokku
-systemctl start uwsgi-bokku
-
-pip3 install bokku --upgrade
-bokku init
-
-#
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
-cp  ~/.ssh/id_rsa.pub /tmp/id_rsa.pub
-
-
-#>>>
-adduser --disabled-password --gecos 'PaaS access' --ingroup www-data bokku
-su - bokku
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-bokku set-ssh /tmp/id_rsa.pub
-exit
-#<<<
+PYTHONWARNINGS="ignore" ansible-playbook -i localhost, playbook.yml
 
 echo ""
 echo "Bokku installation complete!"

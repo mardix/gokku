da835e27f30b9ed9f1a0762233c6db444ade51a3
Mardix <mardix@pylot.io>
2019-10-01 22:43:29 -0400

    Upd

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
 echo "Bokku installer"
 
-apt-get update
-apt-get install -y wget curl cron build-essential certbot git incron \
-   libjpeg-dev libxml2-dev libxslt1-dev zlib1g-dev nginx \
-   python-certbot-nginx python-dev python-pip python-virtualenv \
-   python3-dev python3-pip python3-virtualenv \
-   uwsgi uwsgi-plugin-asyncio-python3 uwsgi-plugin-gevent-python \
-   uwsgi-plugin-python uwsgi-plugin-python3 uwsgi-plugin-tornado-python \ 
-apt-get update
+#apt-get update
+#apt-get install -y wget curl cron build-essential certbot git incron \
+#   libjpeg-dev libxml2-dev libxslt1-dev zlib1g-dev nginx \
+#   python-certbot-nginx python-dev python-pip python-virtualenv \
+#   python3-dev python3-pip python3-virtualenv \
+#   uwsgi uwsgi-plugin-asyncio-python3 uwsgi-plugin-gevent-python \
+#   uwsgi-plugin-python uwsgi-plugin-python3 uwsgi-plugin-tornado-python \
+#apt-get update
 
-pip install ansible
+#pip install ansible
 
-pip3 install bokku
+#pip3 install bokku
 
 cd /tmp
 wget https://raw.githubusercontent.com/mardix/bokku/master/files/playbook.yml
 
-PYTHONWARNINGS="ignore" ansible-playbook -i localhost, playbook.yml
+PYTHONWARNINGS="ignore" ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
 
 echo ""
 echo "Bokku installation complete!"
